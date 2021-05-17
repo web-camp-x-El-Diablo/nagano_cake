ff67191c92e40d7bc0518385cc1c2e2c58ef8b02
kuccho524 <kucchachachanocha@gmail.com>
2021-05-17 10:10:49 +0000

    Revert "Change default of sales_status"
    This reverts commit d0523babdbe4237bb361c666e0d85c2cff972362.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_17_071525) do
+ActiveRecord::Schema.define(version: 2021_05_16_111050) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -96,7 +96,7 @@ ActiveRecord::Schema.define(version: 2021_05_17_071525) do
     t.string "image_id"
     t.text "introduction"
     t.integer "price"
-    t.boolean "sales_status", default: true, null: false
+    t.boolean "sales_status", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
