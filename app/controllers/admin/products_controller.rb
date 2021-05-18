aadf5bb296739e1bf1f1a72f99a361a5fb2f6083
yoshiki0756 <g18lb014.yoshiki@gmail.com>
2021-05-18 14:05:59 +0000

    [create]admin_product_index,new page

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,6 +1,7 @@
 class Admin::ProductsController < ApplicationController
   # 商品のコントローラー
   def index
+    
     @products = Product.page(params[:page]).reverse_order
   end
 
@@ -11,11 +12,21 @@ class Admin::ProductsController < ApplicationController
   end
 
   def new
+    @product = Product.new
   end
 
   def create
+    @product = Product.new(product_params)
+    @product.save
+    redirect_to admin_products_path
   end
 
   def update
   end
+
+  private
+  def product_params
+    params.require(:product).permit(:genre_id, :image, :name, :introduction, :price, :sales_status)
+  end
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,10 @@
 class Product < ApplicationRecord
   # 商品のアソシエーション
-  has_many :genres
+  belongs_to :genre
+  # 商品の画像投稿
+  attachment :image, destroy: false
+  
+  validates :name, presence: true
+  validates :introduction, presence: true
+  validates :price, presence: true
 end
