7149e8017fcb1f9710af9c27300e6dff4a10d8ec
KenInoue1911 <inouetkrk3@gmail.com>
2021-05-18 08:39:28 +0000

    CustomerIsDeleted

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -8,8 +8,18 @@ class Admin::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    @customer.update(customer_params)
+    redirect_to admin_customer_path(@customer.id)
+  end
+
+    private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :postal_code, :address, :telephone_number, :first_name_kana, :email, :is_deleted)
   end
 end
