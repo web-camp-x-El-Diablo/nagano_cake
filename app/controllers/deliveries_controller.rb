9781cba13211a7012406ca50e5a3919070adc0c3
kentakazumi <majorkzmkkzmk@gmail.com>
2021-05-18 01:54:19 +0000

    deliveries_controller

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -1,16 +1,37 @@
 class DeliveriesController < ApplicationController
   def index
+  	@deliveries = Delivery.all
+  	@delivery = Delivery.new
   end
 
-  def edit
+	def create
+		 @delivery = Delivery.new(delivery_params)
+		 @delivery.save
+		 redirect_to deliveries_path
+	end
+
+
+	def edit
+		@delivery = Delivery.find(params[:id])
+	end
+
+	def update
+		@delivery = Delivery.find(params[:id])
+    @delivery.update(delivery_params)
+    @deliveries = Delivery.all
+    @delivery = Delivery.new
+    redirect_to deliveries_path
+	end
+
+	def destroy
+		 @delivery = Delivery.find(params[:id])
+     @delivery.destroy
+     redirect_to deliveries_path
+	end
+
+	private
+  def delivery_params
+    params.require(:delivery).permit(:postal_code, :address, :destination)
   end
-  
-  def create
-  end
-  
-  def update
-  end
-  
-  def destroy
-  end
+
 end
