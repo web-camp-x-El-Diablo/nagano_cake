24e73304506db92a7018f83fa5069377d7af2b74
kuccho524 <kucchachachanocha@gmail.com>
2021-05-18 02:59:43 +0000

    db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-#DB一覧
-
 ActiveRecord::Schema.define(version: 2021_05_17_071525) do
 
   create_table "admins", force: :cascade do |t|
