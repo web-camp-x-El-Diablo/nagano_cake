fb68d75621bb5e0387f616b65406ee214451007a
kentakazumi <majorkzmkkzmk@gmail.com>
2021-05-22 09:41:57 +0000

    cart_products,delivery

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -1,6 +1,9 @@
 class Delivery < ApplicationRecord
   belongs_to :customer
   def all_address
-    '〒'+self.postal_code + self.address + self.destination 
+    '〒'+self.postal_code + self.address + self.destination
   end
+  validates :address, presence: true
+  validates :postal_code, presence: true
+  validates :destination, presence: true
 end
